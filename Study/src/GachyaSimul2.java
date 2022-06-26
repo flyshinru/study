94650ca388766cfcbe40c43a07ad982dcc8d7fc4
flyshinru <107924033+flyshinru@users.noreply.github.com>
2022-06-26 09:46:51 +0900

    4

--- Study/src/GachyaSimul2.java
+++ Study/src/GachyaSimul2.java
@@ -1,11 +1,13 @@
 
-public class GachyaSimul {
+public class GachyaSimul2 {
 	public static void main(String[] args) {
 
 		// 確率
-		double kakuritu = 4;
-		// 枚数
-		int maisuu = 100;
+		double kakuritu = 1.28;
+
+		// 回数
+		int count = 200;
+
 		// 試行数
 		int kaisuu = 10000;
 
@@ -14,33 +16,33 @@ public class GachyaSimul {
 			System.out.println("確率が0なので、終了します。");
 			return;
 		}
-		if (maisuu == 0) {
+		if (count == 0) {
 
-			System.out.println("枚数が0なので、終了します。");
+			System.out.println("回数が0なので、終了します。");
 			return;
 		}
 
-		// 回数
-		int count = 0;
+		// 枚数
+		int maisuu = 0;
 
 		int _kaisuu = 0;
 		while (_kaisuu != kaisuu) {
 
 			// 引けた枚数
-			int _maisuu = 0;
-			while (_maisuu != maisuu) {
+			int _count = 0;
+			while (_count != count) {
 
 				double choice = Math.random()*100;
-				count++;
+				_count++;
 				if (0 <= choice && choice < kakuritu) {
 
-					_maisuu++;
+					maisuu++;
 				}
 			}
 
 			_kaisuu++;
 		}
-		System.out.println("回数：" + count);
-		System.out.println("平均：" + count/kaisuu);
+		System.out.println("枚数：" + maisuu);
+		System.out.println("平均：" + maisuu/kaisuu);
 	}
 }
