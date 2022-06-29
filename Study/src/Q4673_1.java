3e11d5cab7170e548b214af7c01ee6a1401a2900
flyshinru <107924033+flyshinru@users.noreply.github.com>
2022-06-29 21:19:38 +0900

    commit

---
+++ Study/src/Q4673_1.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Q4673_1 {
+	public static void main(String[] args) {
+
+		List<Integer> list = new ArrayList<>();
+		for (int i = 1; i <10001; ++i) {
+
+			if (list.contains(i)) {
+
+				continue;
+			}
+
+			int number = i;
+			while (number < 10001) {
+
+				int temp = number;
+				while(temp > 9) {
+
+					number += temp%10;
+					temp = temp/10;
+				}
+				number += temp%10;
+
+				if (number <= 10000 && !list.contains(number)) {
+
+					list.add(number);
+				}
+
+			}
+		}
+		Collections.sort(list);
+
+		for (int i = 1; i < 10001; i++) {
+
+			if (!list.contains(i)) {
+
+				System.out.println(i);
+			}
+		}
+	}
+}
---
+++ Study/src/Q4673_2.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Q4673_2 {
+	public static void main(String[] args) {
+
+		List<Integer> list = new ArrayList<>();
+		for (int i = 1; i <10001; ++i) {
+
+			int number = i;
+			int temp = number;
+			while(temp > 9) {
+
+				number += temp%10;
+				temp = temp/10;
+			}
+			number += temp%10;
+			if (number <= 10000 && !list.contains(number)) {
+
+				list.add(number);
+			}
+		}
+
+		Collections.sort(list);
+		for (int i = 1; i < 10001; i++) {
+
+			if (!list.contains(i)) {
+
+				System.out.println(i);
+			}
+		}
+	}
+}
